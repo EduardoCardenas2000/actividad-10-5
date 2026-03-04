a7f3e3da0142eb7bae8f49048456fdf47947d47f
EduardoCardenas2000 <eduardocardenas444gmail.com>
2026-03-04 03:06:35 -0600

    terminado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿//Cree un diccionario que almacene usuario y contraseña.
 //Imprima la contraseña de un usuario específico.
+
+Dictionary<string, int> usuarios  = new Dictionary<string, int>();
+
+usuarios.Add("Carlos", 1234);
+usuarios.Add("Josue", 12345);
+usuarios.Add("Armando", 123456);
+
+string nombre = "Carlos";
+int contraseña = usuarios[nombre];
+Console.WriteLine("la contraseña es: " + contraseña);
